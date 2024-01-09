8aa94ba77c5d8ef6c07841496997b1c9a5b98951
SHAYAN <147740430+shani967@users.noreply.github.com>
2024-01-09 16:31:46 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+import inquirer from 'inquirer';
+let answer = await inquirer.prompt([{
+        type: 'number',
+        name: 'num1',
+        message: "Enter first number"
+    },
+    {
+        type: 'number',
+        name: 'num2',
+        message: "Enter second number"
+    },
+    {
+        type: 'list',
+        name: 'operator',
+        message: "Select operator",
+        choices: ['addision', 'subtraction', 'multiplication', 'division']
+    }]);
+switch (answer.operator) {
+    case 'addision':
+        console.log(answer.num1 + answer.num2);
+        break;
+    case 'subtraction':
+        console.log(answer.num1 - answer.num2);
+        break;
+    case 'multiplication':
+        console.log(answer.num1 * answer.num2);
+        break;
+    case 'division':
+        console.log(answer.num1 / answer.num2);
+        break;
+    default:
+        console.log("Invalid operator");
+        break;
+}
